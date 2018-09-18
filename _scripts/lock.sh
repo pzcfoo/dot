71855a3b18e5cf9f55ae9f2ae069e2ede39b66d7
paul <pzcfoo@gmail.com>
2018-09-18 21:29:34 +1000

    i3 lock script

---
+++ _scripts/lock.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+i3lock -i ~/Documents/wallpapers/1920x1080/montana.jpg \
+	--insidecolor=373445ff \
+	--keyhlcolor=d23c3dff \
+	--ringcolor=ff000000 \
+	--line-uses-inside \
+	--separatorcolor=00000000 \
+	--ringvercolor=ffffffff \
+	--indpos "x+80:y+1000" \
+	--radius=20
