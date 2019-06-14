2e0b84cbd2f5afbf84f9950066b74bda6135db40
paul <pzcfoo@gmail.com>
2019-06-14 21:22:39 +1000

    updates

--- _scripts/launch.sh
+++ _scripts/launch.sh
@@ -6,17 +6,23 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 2; done
 
-if type "xrandr"; then
-   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-      if [ "$m" = "eDP1" ]; then
-         # echo $m
-         MONITOR=$m polybar --reload example &
-      fi
-    done
-else
-   polybar --reload example &
-fi
-# MONITOR="eDP1" polybar --reload example
-# polybar --reload example
+# put it on all monitors
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload example &
+done
+
+
+#if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#      if [ "$m" = "eDP1" ]; then
+#         # echo $m
+#         MONITOR=$m polybar --reload example &
+#      fi
+#    done
+#else
+#   polybar --reload example &
+#fi
+#MONITOR="eDP1" polybar --reload example
+#polybar --reload example
 
 echo "Bars launched..."
